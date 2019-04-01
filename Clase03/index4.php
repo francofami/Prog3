4676b5a8354909befc91a9ba0e438827f5d33d62
francofami <31368175+francofami@users.noreply.github.com>
2019-04-01 10:57:51 -0300

    Add files via upload

---
+++ Clase03/index4.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "GET: ";
+echo var_dump($_GET)."<br>";
+echo "POST: ";
+echo var_dump($_POST)."<br>";
+echo "REQUEST: ";
+echo var_dump($_REQUEST)."<br>";
+die();
+
+?>
---
+++ Clase03/ingreso.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<form action="index4.php" method="post">
+  Nombre:<br>
+  <input type="text" name="nombre"><br>
+  Apellido:<br>
+  <input type="text" name="apellido"><br><br>
+  <input type="submit" id="btnEnviar" value="Enviar" />
+  <input type="reset" value="Limpiar" />
+</form>
