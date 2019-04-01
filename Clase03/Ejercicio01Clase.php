8096ffb5a90b2051196b4ab04935cea52912b3f5
francofami <31368175+francofami@users.noreply.github.com>
2019-04-01 12:05:35 -0300

    Add files via upload

--- Clase03/Ejercicio01Clase.php
+++ Clase03/Ejercicio01Clase.php
@@ -1,12 +1,20 @@
 <?php 
 require_once "EjercicioClase.php";  
 
-$persona = new Persona();
+/*$persona = new Persona();
 
 $persona->nombre = "Franco";
 $persona->apellido = "Famiglietti";
 
 $persona->Guardar();
-$persona->Leer();
+$persona->Leer();*/
+
+//echo var_dump(Persona::TraerTodasLasPersonas()); //Es estatico entonces no tengo que crear un objeto
+
+foreach(Persona::TraerTodasLasPersonas() as $per)
+{
+    echo $per->ToString();
+    echo "<br>";
+}
 
 ?>
--- Clase03/EjercicioClase.php
+++ Clase03/EjercicioClase.php
@@ -49,6 +49,40 @@ class Persona
         return $retorno;
     }
 
+    public static function TraerTodasLasPersonas()
+    {
+        $i = 0;
+        $retorno = array();
+
+        $archivo = fopen("Ejercicio01Clase.txt", "r");
+
+        while(!feof($archivo))
+        {
+            $nombreApellido = fgets($archivo);
+
+            if($nombreApellido == "")
+            {
+                continue; //Si hay una linea vacia que itere 
+            }
+            
+            //A partir de un string le paso un carater de separacion ('-' en este caso) me devuelve un array con [0]=nombre [1]=apellido\r\n
+
+            $separado = array();
+            $separado = explode(" - ", $nombreApellido);
+            
+            $personaNueva = new Persona();
+
+            $personaNueva->nombre = $separado[0];
+            $personaNueva->apellido = $separado[1];
+
+            $retorno[$i] = $personaNueva;
+            
+            $i += 1;
+        }
+
+        return $retorno;
+    }
+
 }
 
 ?>
