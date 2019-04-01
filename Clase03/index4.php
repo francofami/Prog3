e735ed659117e7a07ef9c756e1660473d7bc2a57
francofami <31368175+francofami@users.noreply.github.com>
2019-04-01 11:26:54 -0300

    Add files via upload

--- Clase03/index4.php
+++ Clase03/index4.php
@@ -1,11 +1,21 @@
 <?php
 
-echo "GET: ";
+require_once "EjercicioClase.php";
+
+/*echo "GET: ";
 echo var_dump($_GET)."<br>";
 echo "POST: ";
 echo var_dump($_POST)."<br>";
 echo "REQUEST: ";
 echo var_dump($_REQUEST)."<br>";
-die();
+die();*/ 
+
+$persona = new Persona();
+$persona->nombre = $_REQUEST["nombre"];
+$persona->apellido = $_REQUEST["apellido"];
+if(!($persona->Guardar()))
+{
+    echo "Se guardo la persona correctamente.";
+}
 
 ?>
